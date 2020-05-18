4350f6dfba21a12d3ddd1fdd9dc309549b6d8202
ashleshmahule <ashleshmahule15@gmail.com>
2020-05-18 16:09:01 +0530

    something

--- assets/js/index.js
+++ assets/js/index.js
@@ -103,3 +103,23 @@ $('.slider__offset').slick({
     }
   ]
 });
+
+
+var mybutton = document.getElementById("scrollBtn");
+
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
